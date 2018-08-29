91459af772ebdbcbb37e51cec0fa901f01cf2a5f
meirrosendorff <meir.rosendorff@gmail.com>
2018-08-29 23:18:40 +0200

    Finally got the code working with users current location

--- app/src/main/java/com/example/dell_user/dvt_weather_app/Weather.java
+++ app/src/main/java/com/example/dell_user/dvt_weather_app/Weather.java
@@ -1,52 +1,83 @@
 package com.example.dell_user.dvt_weather_app;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 public class Weather extends AppCompatActivity {
 
-    BackgroundStyle localBackground;
-    boolean swap = false;
+    //The main weather Report Object
+    weatherReport report;
+
+    //The context
+    Context context;
+
+    //Manages the location
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        context = this;
 
-//        String theams = readFileToString(R.raw.theams);
+        report = new weatherReport(this, (ConstraintLayout) findViewById(R.id.mainScreenLayout));
 
-        localBackground = new BackgroundStyle(this, (ImageView) findViewById(R.id.screenBackground), findViewById(R.id.layout));
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
 
-        createLayout();
-
+        setReportLocation();
 
     }
 
+    /**
+     * Sets the reports correct location
+     */
+    public void setReportLocation(){
 
-    public void createLayout(){
-
-        LayoutInflater inflater = getLayoutInflater();
-
-        LinearLayout mainLayout = (LinearLayout) findViewById(R.id.futurePredictionsLinearLayout) ;
-
-        dayForcast[] nextWeek = new dayForcast[20];
-
-        for (int i = 0; i<20; i++){
-            nextWeek[i] = new dayForcast(this, (ConstraintLayout) inflater.inflate(R.layout.template, mainLayout, false));
-            mainLayout.addView(nextWeek[i].getLayout());
-
-            nextWeek[i].setTemperature(i);
-            nextWeek[i].setDay(i%7);
-
-            nextWeek[i].setWeather(i%3);
-
-
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+            double lon = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude();
+            double lat = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude();
+            report.setLocation(lat, lon);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
         }
+    }
+
+    /**
+     * Requests users permsiion for location services on installation.
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                double lon = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude();
+                double lat = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude();
+                report.setLocation(lat, lon);
+            }
+        }
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        setReportLocation();
     }
 
 }
